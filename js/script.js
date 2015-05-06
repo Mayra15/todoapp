1cc826305d2eee8f9dbe8e9b3b5b39f557e625b4
bc-1 projects <mayraam9@gmail.com>
2015-05-05 22:25:40 -0700

    added var list, created list items, created element list itemand added text content.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 var addButton = document.querySelector('#add-task');
-var list;
+var list = document.querySelector('#list-items');
 
 addButton.addEventListener('click',addItem);
 
 function addItem(){
-    var newItem = document.querySelector('#task-text').value;
-    li.textContent = (newItem);
-    list.appendChild(li);
+
+    //We're saving the what the user has put as a task. We're creating a new var and saving the user input to it.
+    var userInput = document.querySelector('#task-text').value;
+
+    //We have the user input and we're creating a list item.
+    var listItem = document.createElement('li');
+
+    //give the list item the content from the user (userInput)
+    listItem.textContent = (userInput);
+
+    //we're finally putting the list item onto the HTML (so the user can finally see it).
+    list.appendChild(listItem);
 
 }

c00e1659f18a4cf9dd2b1297324d970f153ddbea
bc-1 projects <mayraam9@gmail.com>
2015-05-09 23:24:47 -0700

    commting what's done so far so alex can help

--- js/script.js
+++ js/script.js
@@ -1,20 +1,71 @@
 var addButton = document.querySelector('#add-task');
 var list = document.querySelector('#list-items');
 
+
 addButton.addEventListener('click',addItem);
 
-function addItem(){
 
-    //We're saving the what the user has put as a task. We're creating a new var and saving the user input to it.
+function addItem() {
+
+    //We're saving the what the user has put as a task and creating a new var and then saving the user input in it.
     var userInput = document.querySelector('#task-text').value;
 
     //We have the user input and we're creating a list item.
     var listItem = document.createElement('li');
+    listItem.classList.add('collection-item')
+    //giving the list item the content from the user (userInput)
+    var idNumber = list.length;
+    var liHTML = [  '<input type="checkbox" class="complete" id="checkbox'+idNumber+'" /> <label for="checkbox'+idNumber+'"> </label> <span>'+ userInput+'</span> ',
+        '<button class="btn waves-effect waves-light delete red right" type="submit" name="action">Delete',
+        '<i class="mdi-action-delete right"></i>',
+        '</button>',
+        '<button class="btn waves-effect waves-light edit right" type="submit" name="action">Edit',
+        '<i class="mdi-action-description right"></i>',
+        '</button>'].join('\n');
+    listItem.innerHTML = liHTML;
 
-    //give the list item the content from the user (userInput)
-    listItem.textContent = (userInput);
-
-    //we're finally putting the list item onto the HTML (so the user can finally see it).
-    list.appendChild(listItem);
+    document.getElementById('list-items').appendChild(listItem);
+    setUpListener()
+}
+//delete function - var delbutton was created in the function but a separate function was made to actually delete the li
+function setUpListener (){
+    var delButtons = document.querySelectorAll('.delete');
+    for (var i = 0; i < delButtons.length; i++){
+       var delButton = delButtons[i];
+        delButton.addEventListener('click', delListItem);
+    }
 
 }
+
+//delete function
+
+function delListItem (){
+
+    this.parentNode.outerHTML = '';
+}
+
+// for the user to be able to edit their li
+function editToDo (){
+    var editButton = document.querySelectorAll('.edit');
+    for (var i = 0; i < editButtons.length; i++){
+        var editButton = editButtons[i];
+        editButton.addEventListener('click', editLi);
+
+// edit function
+
+function editLi(){
+
+    this.parentNode.innerHTML = '<input type="edit"/>' + userInput;
+}
+
+//to mark the list item as done
+
+function doneLi () {
+
+}
+
+
+
+
+
+
